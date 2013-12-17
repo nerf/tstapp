12a503282ab91aa374ad1ce7ea7cdcf9a1107c22
Rosen Tihomirov <rosko@mindsys.org>
2013-12-17 01:03:54 +0000

    register factorygirl and db cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,22 @@
 PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 
+DatabaseCleaner.strategy = :truncation
+
+FactoryGirl.definition_file_paths = [File.join(Padrino.root, 'spec', 'factories')]
+FactoryGirl.find_definitions
+
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
+  conf.include FactoryGirl::Syntax::Methods
+
+  conf.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  conf.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 # You can use this method to custom specify a Rack app
