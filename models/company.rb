3956f967abfb07ccedd43ce1b9d97751faec2a8c
Rosen Tihomirov <rosko@mindsys.org>
2013-12-17 12:23:36 +0000

    validate email,phone only if present

--- models/company.rb
+++ models/company.rb
@@ -1,11 +1,13 @@
 class Company < ActiveRecord::Base
+  EMAIL = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
+  PHONE_NUM = /\A[0-9()+ -]+\z/
 
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
   validates :city, presence: true
   validates :country, presence: true
-  validates :email, format: { with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i }
-  validates :phone, format: { with: /\A[0-9()+ -]+\z/ }
+  validates :email, format: { with: EMAIL }, if: "self.email?"
+  validates :phone, format: { with: PHONE_NUM }, if: "self.phone?"
 
   has_many :owners
 end
